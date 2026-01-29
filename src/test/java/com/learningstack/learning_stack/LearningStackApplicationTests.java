cc33cf3b328a4c760b816101bc8717ff41cd553c
rishabh132003 <rishabhbhandarijain131995@gmail.com>
2026-01-29 20:32:50 +0530

    Basic Initillizer, we had taken some dependencies : Spring Web ,Spring Data JPA ,H2 Database ,Lombok.

---
+++ src/test/java/com/learningstack/learning_stack/LearningStackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learningstack.learning_stack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningStackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
